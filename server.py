9fd9349cc5f04fdbe645199c4df31622e0b9ed60
Peter Thorun <jogueogro@gmail.com>
2023-06-30 17:23:47 -0300

    Finalizada primeira versao

--- server.py
+++ server.py
@@ -4,16 +4,51 @@ import shelve
 app = Flask(__name__)
 app.secret_key = 'pingpong'
 
-jogadores = [
-    {'nome': 'Joaozinho', 'rating': '1100', 'forma': 'W-W-L', 'wins': ['mj', 'mj']},
-    {'nome': 'Israel', 'rating': '1005',  'forma': '', 'wins': ['mj2']},
-    {'nome': 'Marcelo Tenório', 'rating': '1200',  'forma': 'L-L', 'wins': ['mj']},
-]
-
 shelve_file = shelve.open('pingpong')
+
+try:
+    jogadores = shelve_file['jogadores']
+except:
+    jogadores = []
 
 @app.route('/')
 def index():
+    global jogadores
+    jogadores.sort(reverse=True, key=get_rating)
     return render_template('index.html', titulo='Ranqueada do Pingue-Pongue', jogadores=jogadores)
+
+@app.route('/add')
+def adicionar():
+    return render_template('add.html', titulo='Adicionar Levvado')
+
+@app.route('/process_add', methods=['GET', 'POST'])
+def processar_add():
+    nome = request.form['nome']
+    if nome == '':
+        flash('Nome vazio, Zé!', 'error')
+        return redirect(url_for('adicionar'))
+    new_player = {'nome': nome, 'rating': '1000',  'forma': '', 'wins': []}
+    jogadores.append(new_player)
+    shelve_file['jogadores'] = jogadores
+    return redirect(url_for('index'))
+
+@app.route('/post_results', methods = ['GET', 'POST'])
+def get_post_javascript_data():
+    global jogadores
+    vencedor = request.form['vencedor']
+    perdedor = request.form['perdedor']
+    delta = request.form['delta']
+
+    for jogador in jogadores:
+        if jogador['nome'] == vencedor:
+            jogador['rating'] = int(jogador['rating']) + int(delta)
+        if jogador['nome'] == perdedor:
+            jogador['rating'] = int(jogador['rating']) - int(delta)
+
+    shelve_file['jogadores'] = jogadores
+    return redirect(url_for('index'))
+
+def get_rating(jogador):
+    return int(jogador['rating'])
 
 app.run(debug=True)
--- static/functions.js
+++ static/functions.js
@@ -2,9 +2,16 @@ $(document).ready(function() {
 
     $('tbody#tb_escolhas tr').click(function () {
         var tr = $(this).closest('tr').hide().clone();
-        if ($('div#player1').html() == '') $('div#player1').html(tr.find('td.nome').text());
+        if ($('div#player1').html() == '') {
+            $('div#player1').html(tr.find('td.nome').text());
+            $('input#nome1').attr('value', tr.find('td.nome').text())
+            $('input#rating1').attr('value', tr.find('td.rating').text())
+        }
+
         else if ($('div#player2').html() == '') {
             $('div#player2').html(tr.find('td.nome').text());
+            $('input#nome2').attr('value', tr.find('td.nome').text())
+            $('input#rating2').attr('value', tr.find('td.rating').text())
             $('button#enter').show();
         }
         else alert('Ambos os jogadores já foram selecionados. Finalize o desafio antes de iniciar um novo!');
@@ -43,12 +50,47 @@ $(document).ready(function() {
     );
 
     $('#cb1').click(function () {
-        alert('aa');
+        var delta = calcRating($('input#rating1').attr('value'),$('input#rating2').attr('value'));
+        message = $('input#nome1').attr('value')+' ganhou '+delta+' e '+$('input#nome2').attr('value')+' perdeu '+delta;
+        $('#cb1').hide();
+        $('#cb2').hide();
+        $('#enter').show();
+
+        if(confirm(message)) {
+            $.post( "/post_results", {
+                'vencedor': $('input#nome1').attr('value'),
+                'perdedor': $('input#nome2').attr('value'),
+                'delta': delta
+            },
+                function (response) { window.location = "/"; }
+            );
+        }
+
     });
 
     $('#cb2').click(function () {
-        alert('aa');
+        var delta = calcRating($('input#rating2').attr('value'),$('input#rating1').attr('value'));
+        message = $('input#nome2').attr('value')+' ganhou '+delta+' e '+$('input#nome1').attr('value')+' perdeu '+delta;
+        $('#cb1').hide();
+        $('#cb2').hide();
+        $('#enter').show();
+
+        if(confirm(message)) {
+            $.post( "/post_results", {
+                'vencedor': $('input#nome2').attr('value'),
+                'perdedor': $('input#nome1').attr('value'),
+                'delta': delta
+            },
+                function (response) { window.location = "/"; }
+            );
+        }
     });
 
-
 });
+
+function calcRating (r_vencedor, r_perdedor) {
+    var rt = Number(r_vencedor) + Number(r_perdedor);
+    var d = Number(r_perdedor)/rt;
+
+    return Math.round(20*d);
+}
